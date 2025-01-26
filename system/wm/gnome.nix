60ee1c18c525431d571579fc463144e9acececb0
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-01-26 12:41:05 +0530

    added exclusion to some gnome packages

--- system/wm/gnome.nix
+++ system/wm/gnome.nix
@@ -7,4 +7,22 @@
     # Enable the GNOME Desktop Environment.
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
+
+    environment.gnome.excludePackages = with pkgs; [
+        orca
+        evince
+        geary
+        gnome-tour
+        gnome-user-docs
+        baobab
+        epiphany
+        gnome-contacts
+        gnome-logs
+        gnome-maps
+        gnome-music
+        gnome-connections
+        simple-scan
+        yelp
+        gnome-software
+    ];
 }
