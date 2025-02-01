402a324fca48bca1a5444dcba5ad8b5cafd27caf
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-02-01 17:34:27 +0530

    set default browser

--- config/home.nix
+++ config/home.nix
@@ -43,6 +43,17 @@
     #     };
     # };
 
+    # Default Applications
+    xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+            "text/html" = "app.zen_browser.zen.desktop";
+            "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+            "x-scheme-handler/about" = "app.zen_browser.zen.desktop";
+            "x-scheme-handler/unknown" = "app.zen_browser.zen.desktop";
+        };
+    };
+
     home.username = "pranavanath";
     home.homeDirectory = "/home/pranavanath";
     home.stateVersion = "25.05";
