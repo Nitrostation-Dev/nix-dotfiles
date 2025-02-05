430f371f0f9a7d7f637ababf26d3d60f6427c5f7
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-02-05 23:12:29 +0530

    fixed issues with preferences for browser

--- pkgs/home.nix
+++ pkgs/home.nix
@@ -20,4 +20,14 @@
 
             tic-80
         ];
+
+    xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+            "text/html" = "app.zen_browser.zen.desktop";
+            "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+            "x-scheme-handler/about" = "app.zen_browser.zen.desktop";
+            "x-scheme-handler/unknown" = "app.zen_browser.zen.desktop";
+        };
+    };
 }
