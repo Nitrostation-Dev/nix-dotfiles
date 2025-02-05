6d12111b1c95d82e71ee1a65e7e79ae2202c088b
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-02-05 10:52:09 +0530

    fixed issues

--- system/hardware/graphics.nix
+++ system/hardware/graphics.nix
@@ -8,7 +8,7 @@
     };
 
     hardware.nvidia = {
-        modesettings.enable = true;
+        modesetting.enable = true;
         open = false;
         nvidiaSettings = true;
         package = config.boot.kernelPackages.nvidiaPackages.stable;
