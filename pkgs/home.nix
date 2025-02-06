e5b525a0cfa439b8d3d39eba2eadc5ceca612f32
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-02-06 19:25:12 +0530

    changed default browser

--- pkgs/home.nix
+++ pkgs/home.nix
@@ -23,10 +23,10 @@
     xdg.mimeApps = {
         enable = true;
         defaultApplications = {
-            "text/html" = "app.zen_browser.zen.desktop";
-            "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
-            "x-scheme-handler/about" = "app.zen_browser.zen.desktop";
-            "x-scheme-handler/unknown" = "app.zen_browser.zen.desktop";
+            "text/html" = "firefox.desktop";
+            "x-scheme-handler/https" = "firefox.desktop";
+            "x-scheme-handler/about" = "firefox.desktop";
+            "x-scheme-handler/unknown" = "firefox.desktop";
         };
     };
 }
