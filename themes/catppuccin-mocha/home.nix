37e72a3064ad3bdaafc10cf85d4b622c5abfa539
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-02-11 00:43:43 +0530

    :)

--- themes/catppuccin-mocha/home.nix
+++ themes/catppuccin-mocha/home.nix
@@ -1,10 +1,9 @@
 { pkgs, lib, config, userSettings, ... }:
 {
-    # lib.mkIf (userSettings.wm = "hyprland") {
     imports = [
+        ./wm/wmSettings.nix
         ./wm/hyprland.nix
     ];
-    # };
     
     stylix = {
         enable = true;
@@ -53,58 +52,11 @@
         targets.vscode.enable = false;
     };
 
-    # Hyprland Config
     home.pointerCursor = lib.mkForce {
         gtk.enable = true;
         x11.enable = true;
         name = "catppuccin-mocha-dark-cursors";
         package = pkgs.catppuccin-cursors.mochaDark;
         size = 28;
-    };
-
-    options = with lib; with types; {
-        hyprland_theme_settings = mkOption { type = str; };
-    };
-    config = {
-        hyprland_theme_settings = ''
-# LOOK AND FEEL
-general {
-    gaps_in = 4 
-    gaps_out = 10
-
-    border_size = 2
-
-    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-    col.inactive_border = rgba(595959aa)
-
-    resize_on_border = false
-
-    allow_tearing = false
-
-    layout = dwindle
-}
-
-decoration {
-    rounding = 10
-
-    active_opacity = 1.0
-    inactive_opacity = 1.0
-
-    shadow {
-        enabled = true
-        range = 4
-        render_power = 3
-        color = rgba(1a1a1aee)
-    }
-
-    blur {
-        enabled = true
-        size = 3
-        passes = 1
-
-        vibrancy = 0.1696
-    }
-}
-        '';
     };
 }
---
+++ themes/catppuccin-mocha/wm/wmSettings.nix
@@ -0,0 +1,48 @@
+{ config, lib, userSettings, ... }:
+{
+    options = with lib; with types; {
+        hyprland_theme_settings = mkOption { type = str; };
+    };
+    config = {
+        hyprland_theme_settings = ''
+# LOOK AND FEEL
+general {
+    gaps_in = 4 
+    gaps_out = 10
+
+    border_size = 2
+
+    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
+    col.inactive_border = rgba(595959aa)
+
+    resize_on_border = false
+
+    allow_tearing = false
+
+    layout = dwindle
+}
+
+decoration {
+    rounding = 10
+
+    active_opacity = 1.0
+    inactive_opacity = 1.0
+
+    shadow {
+        enabled = true
+        range = 4
+        render_power = 3
+        color = rgba(1a1a1aee)
+    }
+
+    blur {
+        enabled = true
+        size = 3
+        passes = 1
+
+        vibrancy = 0.1696
+    }
+}
+        '';
+    };
+}
